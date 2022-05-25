038f935665a3abc78e7cc718764b7169449ecfc5
bethanyshoun <bethanyshoun@gmail.com>
2022-05-24 23:04:48 -0400

    added preliminary High Score and Timer divs to HTML, started stylesheet, pseudocoded javascript with comments

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Start Button
+
+//Timer begins counting back when start button is pushed
+
+// Question with answer choices appears
+
+
+//IF answer is incorrect time is subtracted from the clock AND INCORRECT appears on page
+
+//If answer is correct then CORRECT appears on page
+
+//Subsequent questions appear with same conditions
+
+//When all questions are answered OR the timeer reaches 0 the game is over
+
+//The score is the amount of time left on the clock
+
+//PLayer initials and score can be saved (HIGH SCORE)
