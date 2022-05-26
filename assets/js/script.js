6172994d32b414daafabc75489708e4eff66bf60
bethanyshoun <bethanyshoun@gmail.com>
2022-05-25 20:04:34 -0400

    Create quiz start page and begin questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
-var countdown 
+var countdown; 
+var questions =[
+    {
+        header:"Question 1",
+        choices: [
+            "choice 1",
+            "choice 2",
+            "choice 3",
+            "choice 4"
+        ]
+    },
+    {
+        header:"Question 2",
+        choices: [
+            "choice 1",
+            "choice 2",
+            "choice 3",
+            "choice 4"
+        ]
+    }
+];
+var showQuestions = function() {
+    console.log(questions[0].header);
+}
 
 //Start Button
+var startButton = document.getElementById("startBtn")
+    //console.log(startButton);
+
+startButton.addEventListener("click", function() {
+    var startPage = document.querySelector(".start-page")
+    
+    startPage.classList.add("hide")
+
+    var quizSection = document.getElementById("quiz")
+        console.log(quizSection);
+        //remove HIDE from quiz
+        quizSection.classList.remove("hide")
+
+showQuestions();
+
+})
+
 
 //Timer begins counting back when start button is pushed
 
